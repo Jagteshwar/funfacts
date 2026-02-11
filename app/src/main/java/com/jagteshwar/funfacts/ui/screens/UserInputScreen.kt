cb3c58be664b89c7aa899b84f487074c51ad1d68
jagteshwar <jagteshwars3@gmail.com>
2026-02-11 12:21:52 -0500

    navigation graph

--- app/src/main/java/com/jagteshwar/funfacts/ui/screens/UserInputScreen.kt
+++ app/src/main/java/com/jagteshwar/funfacts/ui/screens/UserInputScreen.kt
@@ -1,6 +1,9 @@
 package com.jagteshwar.funfacts.ui.screens
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
-fun UserInputScreen(){}
+fun UserInputScreen(modifier: Modifier){
+
+}
