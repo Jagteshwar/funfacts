daa36be093dbbf756989f29714d8186ac84248b8
jagteshwar <jagteshwars3@gmail.com>
2026-02-12 12:37:28 -0500

    user input screen WIP

--- app/src/main/java/com/jagteshwar/funfacts/ui/screens/UserInputScreen.kt
+++ app/src/main/java/com/jagteshwar/funfacts/ui/screens/UserInputScreen.kt
@@ -1,9 +1,25 @@
 package com.jagteshwar.funfacts.ui.screens
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun UserInputScreen(modifier: Modifier){
+fun UserInputScreen(){
+    Surface(
+        modifier = Modifier.fillMaxSize().height(80.dp)
+    ) {
+        Text(text = Routes.USER_INPUT_SCREEN)
+    }
+}
 
+@Preview
+@Composable
+fun UserInputScreenPreview() {
+    UserInputScreen()
 }
